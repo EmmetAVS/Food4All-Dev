f7e1094c621a6dd39b75fc4d84cc9a0761e682ea
EmmetAVS <aarush.shrikanth@gmail.com>
2025-06-14 18:29:52 -0700

    work on modal, bug fixes, started work on js

---
+++ static/scripts/view_collections.js
@@ -0,0 +1,35 @@
+function animateOpenModal(m) {
+  m.style.display = ""
+  m.style.opacity = "1"
+  m.style.background = "rgba(0, 0, 0, 0.8)"
+  m.style.animation = "fade-in 0.3s"
+  m.children[0].style.animation = "move-up 0.3s"
+}
+
+function animateCloseModal(m) {
+  setTimeout(function() {
+    m.style.display = "none"
+  }, 301)
+  m.children[0].style.animation = "move-down 0.3s"
+  m.style.animation = "fade-out 0.3s"
+  m.style.opacity = 0
+}
+
+function modalClose(str) {
+    const modal = document.getElementById("modal");
+
+    if (!modal) return;
+
+    if (str == "use") {
+        //collect data
+    }
+
+    animateCloseModal(modal);
+}
+
+function main() {
+    animateOpenModal(document.getElementById("modal"));
+    console.log("View Collections script loaded");
+}
+
+window.onload = main;
